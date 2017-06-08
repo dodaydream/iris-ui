69df74646a8c891fea6d4b7d6bde0c6f2486304b
Stanley Cao <daydream2013@live.com>
2017-06-08 21:54:40 +0800

    Add placeholder

---
+++ src/irInput/irInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input class="ir-input" type="text" placeholder="{this.placeholder}"></input>
+</template>
+
+<style lang="less">
+@import "../less/palette.less";
+.ir-input {
+  height: 2em;
+  border: 1px solid #ccc;
+  border-radius: 1px;
+  transition: .25s;
+  &:focus, &:hover {
+    border-color: @primary-color;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: ['placeholder']
+}
+</script>
